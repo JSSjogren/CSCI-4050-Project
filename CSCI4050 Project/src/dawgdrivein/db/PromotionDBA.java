833acaa55ea2d79b4419e7e6c268c4b340249489
Jarod Sjogren <jss29576@uga.edu>
2018-03-31 17:32:00 -0400

    Created Promo, Users, & Movie DBA files
    Includes add, update, and delete for Promo, Users (Manager, Employee,
    RegisteredCustomer), and Movie objects.

--- CSCI4050 Project/src/dawgdrivein/db/PromotionDBA.java
+++ CSCI4050 Project/src/dawgdrivein/db/PromotionDBA.java
@@ -1,5 +1,11 @@
 package dawgdrivein.db;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 import dawgdrivein.entity.Promotion;
 
 public class PromotionDBA {
@@ -7,11 +13,53 @@ public class PromotionDBA {
 	
 	public boolean createPromo(Promotion promo)
 	{
-		return true;
+		try {
+            // 1. configuring hibernate
+            Configuration configuration = new Configuration().configure();
+ 
+            // 2. create sessionfactory
+            SessionFactory sessionFactory = configuration.buildSessionFactory();
+ 
+            // 3. Get Session object
+            Session session = sessionFactory.openSession();
+ 
+            // 4. Starting Transaction
+            Transaction transaction = session.beginTransaction();
+            session.save(promo);
+            transaction.commit();
+            System.out.println("\n\n Details Added \n");
+            return true;
+ 
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            System.out.println("error");
+            return false;
+        }
 	}
 	
 	public boolean removePromo(Promotion promo)
 	{
-		return true;
+		try {
+            // 1. configuring hibernate
+            Configuration configuration = new Configuration().configure();
+ 
+            // 2. create sessionfactory
+            SessionFactory sessionFactory = configuration.buildSessionFactory();
+ 
+            // 3. Get Session object
+            Session session = sessionFactory.openSession();
+ 
+            // 4. Starting Transaction
+            Transaction transaction = session.beginTransaction();
+            session.save(promo);
+            transaction.commit();
+            System.out.println("\n\n Details Removed \n");
+            return true;
+ 
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            System.out.println("error");
+            return false;
+        }
 	}
 }
