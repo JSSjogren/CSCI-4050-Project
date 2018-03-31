3618078c9b3abc7fa2b1ca3fc4ae8631976a4d1f
Jarod Sjogren <jss29576@uga.edu>
2018-03-31 18:00:39 -0400

    Updated Hibernate xml files and create Hall/HallDBA classes

---
+++ CSCI4050 Project/src/dawgdrivein/db/HallDBA.java
@@ -0,0 +1,63 @@
+package dawgdrivein.db;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import dawgdrivein.entity.Hall;
+
+public class HallDBA {
+
+	public boolean createHall(Hall hall)
+	{
+		try {
+            // 1. configuring hibernate
+            Configuration configuration = new Configuration().configure();
+ 
+            // 2. create sessionfactory
+            SessionFactory sessionFactory = configuration.buildSessionFactory();
+ 
+            // 3. Get Session object
+            Session session = sessionFactory.openSession();
+ 
+            // 4. Starting Transaction
+            Transaction transaction = session.beginTransaction();
+            session.save(hall);
+            transaction.commit();
+            System.out.println("\n\n Details Saved \n");
+            return true;
+ 
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            System.out.println("error");
+            return false;
+        }	
+	}
+	
+	
+	public void updateAvailable(Hall hall)
+	{
+		try {
+            // 1. configuring hibernate
+            Configuration configuration = new Configuration().configure();
+ 
+            // 2. create sessionfactory
+            SessionFactory sessionFactory = configuration.buildSessionFactory();
+ 
+            // 3. Get Session object
+            Session session = sessionFactory.openSession();
+ 
+            // 4. Starting Transaction
+            Transaction transaction = session.beginTransaction();
+            session.saveOrUpdate(hall);
+            transaction.commit();
+            System.out.println("\n\n Details Updated \n");
+ 
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            System.out.println("error");
+        }
+	}
+}
