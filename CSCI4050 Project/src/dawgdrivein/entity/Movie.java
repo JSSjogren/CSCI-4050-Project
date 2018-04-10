406b37b86bed3fdd00790ef772fe4d07f27bef49
Jarod Sjogren <jss29576@uga.edu>
2018-04-10 12:34:44 -0400

    Sign-in/registration appear to function again. Fingers crossed.

--- CSCI4050 Project/src/dawgdrivein/entity/Movie.java
+++ CSCI4050 Project/src/dawgdrivein/entity/Movie.java
@@ -106,4 +106,84 @@ public class Movie {
 	{
 		return movieDBA.retrieveMovie(id);
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getCast() {
+		return cast;
+	}
+
+	public void setCast(String cast) {
+		this.cast = cast;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public String getProducer() {
+		return producer;
+	}
+
+	public void setProducer(String producer) {
+		this.producer = producer;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getTrailer_picture() {
+		return trailer_picture;
+	}
+
+	public void setTrailer_picture(String trailer_picture) {
+		this.trailer_picture = trailer_picture;
+	}
+
+	public String getTrailer_video() {
+		return trailer_video;
+	}
+
+	public void setTrailer_video(String trailer_video) {
+		this.trailer_video = trailer_video;
+	}
+
+	public String getMPAA_rating() {
+		return MPAA_rating;
+	}
+
+	public void setMPAA_rating(String mPAA_rating) {
+		MPAA_rating = mPAA_rating;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
 }
